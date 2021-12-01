de1b36b8568b06e71a9b06190e8418301cafd2aa
Luis Enrique Guerrero <luisenguerrero@yahoo.com>
2021-11-30 23:07:54 -0500

    Fix Error on Tareas Schema, description field

--- schemas/tarea_schemas.js
+++ schemas/tarea_schemas.js
@@ -3,13 +3,12 @@ const { Schema } = mongoose;
 
 const esquema = new Schema({
     titulo: String,
-    descripción: String,
+    descripcion: String,
     entregada: Boolean,
     calificacion: Number,
     fecha_entrega: {type: Date, required: false},
     fecha_vencimiento: Date,
-    materia:
-        { _id: String }
+    materia: String
 });
 
 module.exports = mongoose.model('Tarea', esquema);
